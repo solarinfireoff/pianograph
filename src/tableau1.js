a6c07394e385f10bbb7fc02b9a3d1341a59354b7
solainfire <gabrieldalvai1@gmail.com>
2021-12-06 15:36:53 +0100

    ttttt

--- src/tableau1.js
+++ src/tableau1.js
@@ -2,7 +2,7 @@
  * ALGO: ceci est une classe...
  * Vous verrez ça plus tard en détail avec Rémi, pour l'instant on n'a pas trop besoin de savoir à quoi ça sert.
  */
-class Tableau1 extends Phaser.Scene{
+class tableau1 extends Phaser.Scene{
     /**
      * Précharge les assets
      */
